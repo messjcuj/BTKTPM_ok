03cc539c27c0ca096feefdd8248f5c6dc6564aa4
Your Name <tuanphuc2k@gmail.com>
2021-10-30 14:25:28 +0700

    ok

---
+++ src/main/java/com/example/btktpm/BtktpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.btktpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtktpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtktpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btktpm/BtktpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btktpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtktpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
